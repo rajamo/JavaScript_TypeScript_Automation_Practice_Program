8544ba33a792fa4273d2d59a3fb21b36ff2acc68
Raja-Mohan <raja21.mohan@gmail.com>
2026-02-17 23:07:19 +0000

    variables let const var

--- JavaScript_Program/1_variable.js
+++ JavaScript_Program/1_variable.js
@@ -1,11 +1,11 @@
 
 
 
-///JavaScript have only 2 Variables Let and const.  Var (depricated)
-// Let is uesed to when variable value changes and not allowed RE Declarion  // Hoisting is Allowed with RT Error 
-// Const is used when variable value is constant but object/array properties can be changed even if the object/array is declared as const
-// Const - Allway Declare as CAPITAL - like URL, Days, PI etc., 
-// Var is depricated, avoid using var and ALLOWD to Re declared many time. // Hoisting allowed without RT Error 
+/*JavaScript have only 2 Variables Let and const.  Var (depricated)
+Let is uesed to when variable value changes and not allowed RE Declarion  // Hoisting is Allowed with RT Error 
+Const is used when variable value is constant but object/array properties can be changed even if the object/array is declared as const
+Const - Allway Declare as CAPITAL - like URL, Days, PI etc., 
+ Var is depricated, avoid using var and ALLOWD to Re declared many time. // Hoisting allowed without RT Error */
 
 console.log('********** Let  **********************');
 for (let i=0; i<5; i++){

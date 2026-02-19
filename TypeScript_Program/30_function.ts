fa16267765025dc39b11a1b625d5d8e4876caf4a
Raja Mohan <50270493+rajamo@users.noreply.github.com>
2026-02-19 20:04:15 +0000

    Refactor comments on function return typesTypeScript Functions and Retrun Types 1.Void  2.Never 3.int 4.any
    Updated comments for clarity and consistency in function return types.
    
    Signed-off-by: Raja Mohan <50270493+rajamo@users.noreply.github.com>

--- TypeScript_Program/30_function.ts
+++ TypeScript_Program/30_function.ts
@@ -1,29 +1,35 @@
-//
-//6. function return nothing (void)
-//void type represents the absence of any value
-//typically used as the return type of functions that do not return a value
-// Example of void type:
+//********* TypeScript Functions and Retrun Types 1.Void 2.Never 3.int 4.any ****************
+
+/* 1.function return Type = nothing (void)
+void type represents the absence of any value
+typically used as the return type of functions that do not return a value
+Example of void type: */
+
 function logMessage(message: string): void {    
     console.log(message);
 }
 logMessage('This is a void function'); //void function usage
 
 
-//8. Never 
-//never type represents values that never occur 
-//e.g., a function that always throws an error or never returns
-// Example of never type:
+/* 2.function Retrun Type = Never 
+never type represents values that never occur 
+e.g., a function that always throws an error or never returns
+Example of never type:*/
+
 function throwError(message: string): never {
     throw new Error(message);
 } 
 
-//int function with parameters and return type
+/* 3.function Return Type = int 
+Example : int function with parameters and return type */
+
 function calculas(x:number,y:number):number {
      return x+y;
-
 }
 
-//Any function with parameters and return type
+/*4. function Return Type = any
+Any function with parameters and return type */
+
 function addNumStr(x:number | string,y:number | string):any {
     if(typeof x === 'number' && typeof y === 'number'){ 
     return x+y;
